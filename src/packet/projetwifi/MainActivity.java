64b001008d39f59639de253a48e3edfb72da3bdc
lnans <nans.laupretre@gmail.com>
2014-06-27 12:03:36 +0200

    Update MainActivity.java

--- src/packet/projetwifi/MainActivity.java
+++ src/packet/projetwifi/MainActivity.java
@@ -1,9 +1,7 @@
 package packet.projetwifi;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.StringTokenizer;
-
 import android.net.wifi.*;
 import android.os.Bundle;
 import android.app.Activity;
@@ -19,22 +17,26 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 	
-	Button b1;
-	Button[] Bconnec = new Button[50];
+	Button b1; //Bouton Scan
+	Button[] Bconnec = new Button[50]; //liste de bouton
 	LinearLayout llt;
 	ScrollView llw;
 	WifiManager wifi;
 	TextView txt;
-	Builder pop;
+	Builder pop; //Fenetre pop-up
 	int i;
+	Toast test; 
+	String selec;
+	EditText input;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		//Activation du Wifi
 		wifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
 		if(!wifi.isWifiEnabled()){
 		if(wifi.getWifiState() != WifiManager.WIFI_STATE_ENABLING){
@@ -42,7 +44,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
 			}
 		}
 		
-
+		input = new EditText(this); //Champt de saisie
+		input.setText("");
+		test = new Toast(this); //Toast
 		pop = new AlertDialog.Builder(this);
 		llw = new ScrollView(this);
 		
@@ -69,7 +73,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 	}
 	
 	public void onClick(View v) {
-		
+		//Bouton Scan
 		if(v.getId() == 100){
 		
 		llt.removeAllViews();
@@ -82,7 +86,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		i=0;
 		List<ScanResult> results = wifi.getScanResults();
 		for (ScanResult result : results){
-			String info = String.format("%s\n - BSSID: %s\n - Power: %sdB\n - Auth: %s\n  - Freq: %sMHz", result.SSID,result.BSSID,result.level,result.capabilities,result.frequency);
+			String info = String.format("%s:\nBSSID: %s\nPower: %sdB\nAuth: -%s-\nFreq: %sMHz", result.SSID,result.BSSID,result.level,result.capabilities,result.frequency);
 			Bconnec[i] = new Button(this);
 			Bconnec[i].setId(i);
 			Bconnec[i].setText(info);
@@ -96,27 +100,55 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		setContentView(llw);
 		
 		}
+		//Bouton réseaux Wifi
 		else{
+			//recuperation du SSID
+			String stat = "" + Bconnec[v.getId()].getText();
+			StringTokenizer st1 = new StringTokenizer(stat, ":");
+			final String Ssid = st1.nextToken();
+			//recuperation type Authentification
+			StringTokenizer st2 = new StringTokenizer(stat, "-");
+			String[] stats;
+			stats = new String[10];
+			i=0;
+			while(st2.hasMoreTokens()){
+				stats[i] = st2.nextToken();
+				i++;
+			}
 			
-			String sta = "" + Bconnec[v.getId()].getText();
-			StringTokenizer st = new StringTokenizer(sta, "-");
-			String Ssid = st.nextToken();
+			String Auth = stats[2]; //Authentification en case 2
 			
-			pop.setTitle("" + Ssid);
-			pop.setMessage("Mot de passe :");
-			final EditText input = new EditText(this);
+			pop.setTitle("" + Ssid + " - " + Auth); //Titre dialog
+			pop.setMessage("Pass (si nécessaire):");
 			pop.setView(input);
 			pop.setPositiveButton("Connexion", new DialogInterface.OnClickListener() {
-				
+				//Bouton Connexion
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
-					// connexion
+					Context con = getApplicationContext();
+					String tos = "Connexion...";
+					test.makeText(con, tos, test.LENGTH_SHORT).show();
+					//Connexion à un réseaux ouvert
+					WifiConfiguration conf = new WifiConfiguration();					
+					conf.SSID = "\"" + Ssid + "\"";
+					conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+				
+					WifiManager wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+					wifiManager.addNetwork(conf);
+					
+					List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+					for( WifiConfiguration i : list ) {
+					    if(i.SSID != null && i.SSID.equals("\"" + Ssid + "\"")) {
+					         wifiManager.disconnect();
+					         wifiManager.enableNetwork(i.networkId, true);
+					         wifiManager.reconnect();               
+
+					         break;
+					    }           
+					 }
 					
 				}
-			});
-			pop.show();	
+			});pop.show();	
 		}
 	}
-	
-
 }
